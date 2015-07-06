796d08dbe921f4858aaed20c7a549841865dc47c
Eugenio Paolantonio (g7) <me@medesimo.eu>
2015-07-06 15:43:15 +0200

    [setup] Added.
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# libchannels - update channels management library
+# Copyright (C) 2015 Eugenio "g7" Paolantonio
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
+from distutils.core import setup
+
+setup(name='libchannels',
+	version='0.1.0',
+	description='Update channels management library',
+	author='Eugenio Paolantonio',
+	author_email='me@medesimo.eu',
+	url='https://github.com/semplice/libchannels',
+	packages=[
+		"libchannels"
+      ],
+	requires=['sys', 'enum', 'os', 'configparser', 'aptsources.sourceslist']
+)
+
